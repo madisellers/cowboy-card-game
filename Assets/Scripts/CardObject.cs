bb05d22165fe0f20682c6bcf307c4f7e0c33bce3
madisellers <madisonsellers02@gmail.com>
2026-03-21 14:50:11 -0400

    card set up function

--- Assets/Scripts/CardObject.cs
+++ Assets/Scripts/CardObject.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 
 public class CardObject : MonoBehaviour
 {
-    [SerializeField] private Sprite logoPos1;
-    [SerializeField] private Sprite logoPos2;
+    [SerializeField] private SpriteRenderer logoPos1;
+    [SerializeField] private SpriteRenderer logoPos2;
+    [SerializeField] private SpriteRenderer logoPos3;
     [SerializeField] private TextMeshPro rank1;
     [SerializeField] private TextMeshPro rank2;
 
-    [SerializeField] private Sprite background;
     [SerializeField] private Sprite heartLogo;
     [SerializeField] private Sprite spadeLogo;
     [SerializeField] private Sprite clubLogo;
@@ -16,6 +16,35 @@ public class CardObject : MonoBehaviour
 
     public void SetUp(CardRank rank, CardSuit suit)
     {
+        switch (suit)
+        {
+            case CardSuit.NULL:
+                break;
+            case CardSuit.Hearts:
+                logoPos1.sprite = heartLogo;
+                logoPos2.sprite = heartLogo;
+                logoPos3.sprite = heartLogo;
+                break;
+            case CardSuit.Diamonds:
+                logoPos1.sprite = diamondLogo;
+                logoPos2.sprite = diamondLogo;
+                logoPos3.sprite = diamondLogo;
+                break;
+            case CardSuit.Spades:
+                logoPos1.sprite = spadeLogo;
+                logoPos2.sprite = spadeLogo;
+                logoPos3.sprite = spadeLogo;
+                break;
+            case CardSuit.Clubs:
+                logoPos1.sprite = clubLogo;
+                logoPos2.sprite = clubLogo;
+                logoPos3.sprite = clubLogo;
+                break;
+            default:
+                break;
+        }
 
+        rank1.text = rank.ToString();
+        rank2.text = rank.ToString();
     }
 }
