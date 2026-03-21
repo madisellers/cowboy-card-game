56e80ffc92adabff4407841f69660d5a18feb149
madisellers <madisonsellers02@gmail.com>
2026-03-21 13:28:21 -0400

    add basic stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    void Awake()
+    {
+        //Create instance
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Types.cs
@@ -0,0 +1,90 @@
+public enum CardSuit
+{
+    NULL,
+    Hearts,
+    Diamonds,
+    Spades,
+    Clubs
+}
+
+public enum CardRank
+{
+    NULL,
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King
+}
+
+public enum CheatType
+{
+    NULL,
+    AddCard,
+    WithheldWhenAsked
+}
+
+public struct Card
+{
+    public CardSuit suit;           //This cards suit
+    public CardRank rank;           //This cards rank
+    public bool fromCheatDeck;      //True if this card was added from the cowboy's OWN cheat deck, false otherwise. If given to lead, auto reset to false (kind of like money laundering)
+
+    public bool isNull => (suit == CardSuit.NULL || rank == CardRank.NULL);	//Check if card is null, easy to check for errors
+
+
+    public Card(CardSuit suit, CardRank rank, bool fromCheatDeck)
+    {
+        this.suit = suit;
+        this.rank = rank;
+        this.fromCheatDeck = fromCheatDeck;
+    }
+
+    public string ToString()
+    {
+        return $"({rank} {suit}" + (fromCheatDeck ? "CHEAT" : "") + ")";
+    }
+}
+
+public enum TurnPhase
+{
+    RequestCard,
+    AnswerRequest,
+    TakeCard,
+    PlacePair
+}
+
+/*
+ * TURN PHASE LOOP + WHAT COWBOYS CAN DO DURING EACH (Turn lead (L) vs opposite (O))
+ * ---------------------------------------------------
+ * RequestCard
+ * 		-L: Select card and ask opposite for selected rank, add cheat card
+ * 		-O: Add cheat card
+ * AnswerRequest
+ *		-L: Add cheat card
+ *		-O: Select request answer, add cheat card
+ * TakeCard:
+ *		-L: Take opposite's given card or go fish
+ *		-O: Add cheat card
+ * PlacePair:
+ *		-L: Select two cards to place down as pair or select end turn
+ *		-O: Add cheat card
+ */
+
+enum Difficulty
+{
+    Easy,
+    Normal,
+    Hard
+}
+
+
+
